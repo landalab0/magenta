b9b7a6de4ef28e996dad14c2d2428026da0bc7b1
landalab0 <139167629+landalab0@users.noreply.github.com>
2023-08-11 12:46:05 -0600

    Se cambio nombre de carpeta alojadora de scrips a "magenta"

--- MAGENTA.sh
+++ MAGENTA.sh
@@ -6,13 +6,13 @@ mkdir /databases/MAGENTA/MAGENTA_DB
 
 mkdir /databases/MAGENTA/MAGENTA_DB/Data /databases/MAGENTA/MAGENTA_DB/Metadata /databases/MAGENTA/MAGENTA_DB/Scripts
 
-chmod a+x /databases/MAGENTA/MAGENTA_EXE/Search_and_ClassifScript.sh /databases/MAGENTA/MAGENTA_EXE/DownloadScript.sh /databases/MAGENTA/MAGENTA_EXE/VerificationScript.sh
+chmod a+x /databases/MAGENTA/magenta/Search_and_ClassifScript.sh /databases/MAGENTA/MAGENTA_EXE/DownloadScript.sh /databases/MAGENTA/MAGENTA_EXE/VerificationScript.sh
 
-nohup sh /databases/MAGENTA/MAGENTA_EXE/Search_and_ClassifScript.sh  > /databases/MAGENTA/MAGENTA_EXE/Search_and_Classif_nohup 2>&1 &
+nohup sh /databases/MAGENTA/magenta/Search_and_ClassifScript.sh  > /databases/MAGENTA/MAGENTA_EXE/Search_and_Classif_nohup 2>&1 &
 
-nohup sh /databases/MAGENTA/MAGENTA_EXE/DownloadScript.sh  > /databases/MAGENTA/MAGENTA_EXE/Download_nohup 2>&1 &
+nohup sh /databases/MAGENTA/magenta/DownloadScript.sh  > /databases/MAGENTA/MAGENTA_EXE/Download_nohup 2>&1 &
 
-nohup sh /databases/MAGENTA/MAGENTA_EXE/VerificationScript.sh  > /databases/MAGENTA/MAGENTA_EXE/Verification_nohup 2>&1 &
+nohup sh /databases/MAGENTA/magenta/VerificationScript.sh  > /databases/MAGENTA/MAGENTA_EXE/Verification_nohup 2>&1 &
 
 
 
