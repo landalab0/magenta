7bf7bebce16cbc7b87f8eb5b73e0e6b2b77f5b8a
landalab0 <139167629+landalab0@users.noreply.github.com>
2023-08-25 11:45:10 -0600

    Update MAGENTA.sh

--- MAGENTA.sh
+++ MAGENTA.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-path=/databases
+path=/databases/MAGENTA
 
-mkdir $path/MAGENTA/MAGENTA_DB
+mkdir $path/MAGENTA_DB
 
-mkdir $path/MAGENTA/MAGENTA_DB/Data $path/MAGENTA/MAGENTA_DB/Metadata $path/MAGENTA/MAGENTA_DB/Scripts
+mkdir $path/MAGENTA_DB/Data $path/MAGENTA_DB/Metadata $path/MAGENTA_DB/Scripts
 
-chmod a+x $path/MAGENTA/magenta/Search_and_ClassifScript.sh $path/MAGENTA/magenta/DownloadScript.sh $path/MAGENTA/magenta/VerificationScript.sh
+chmod a+x $path/magenta/Search_and_ClassifScript.sh $path/magenta/DownloadScript.sh $path/magenta/VerificationScript.sh
 
-nohup sh $path/MAGENTA/magenta/Search_and_ClassifScript.sh  > $path/MAGENTA/magenta/Search_and_Classif_nohup 2>&1 & && nohup sh $path/MAGENTA/magenta/DownloadScript.sh  > $path/MAGENTA/magenta/Download_nohup 2>&1 & && nohup sh $path/MAGENTA/magenta/VerificationScript.sh  > $path/MAGENTA/magenta/Verification_nohup 2>&1 &
+nohup sh $path/magenta/Search_and_ClassifScript.sh  > $path/magenta/Search_and_Classif_nohup 2>&1 & && nohup sh $path/magenta/DownloadScript.sh  > $path/magenta/Download_nohup 2>&1 & && nohup sh $path/magenta/VerificationScript.sh  > $path/magenta/Verification_nohup 2>&1 &
 
 
 
