8d0ec634701cdd9795f59ab8060caa58d9240cc7
landalab0 <139167629+landalab0@users.noreply.github.com>
2023-08-25 11:26:53 -0600

    Update MAGENTA.sh "&&"
    Se agrego "&&" entre los nohup's para evitar  que corran simultáneamente

--- MAGENTA.sh
+++ MAGENTA.sh
@@ -6,11 +6,7 @@ mkdir /databases/MAGENTA/MAGENTA_DB/Data /databases/MAGENTA/MAGENTA_DB/Metadata 
 
 chmod a+x /databases/MAGENTA/magenta/Search_and_ClassifScript.sh /databases/MAGENTA/magenta/DownloadScript.sh /databases/MAGENTA/magenta/VerificationScript.sh
 
-nohup sh /databases/MAGENTA/magenta/Search_and_ClassifScript.sh  > /databases/MAGENTA/magenta/Search_and_Classif_nohup 2>&1 &
-
-nohup sh /databases/MAGENTA/magenta/DownloadScript.sh  > /databases/MAGENTA/magenta/Download_nohup 2>&1 &
-
-nohup sh /databases/MAGENTA/magenta/VerificationScript.sh  > /databases/MAGENTA/magenta/Verification_nohup 2>&1 &
+nohup sh /databases/MAGENTA/magenta/Search_and_ClassifScript.sh  > /databases/MAGENTA/magenta/Search_and_Classif_nohup 2>&1 & && nohup sh /databases/MAGENTA/magenta/DownloadScript.sh  > /databases/MAGENTA/magenta/Download_nohup 2>&1 & && nohup sh /databases/MAGENTA/magenta/VerificationScript.sh  > /databases/MAGENTA/magenta/Verification_nohup 2>&1 &
 
 
 
